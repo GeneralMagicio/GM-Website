8e56700c29339220ba9ad7a4b9e7ff9603a30328
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-08-12 20:24:51 -0300

    add fonts and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,28 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    fontFamily: {},
+    fontFamily: {
+      sans: ['Space Grotesk', 'sans-serif'],
+      akira: ['Akira', 'sans-serif'],
+    },
     extend: {
       colors: {
         neutral: {
+          600: '#959DB2',
           800: '#3E4557',
           900: '#161718',
         },
+        magicPurple: {
+          300: '#A569FF',
+          500: '#523480',
+        },
+        pinkPotion: {
+          300: '#FE61A7',
+        },
       },
-      backgroundImage: (theme) => ({}),
+      backgroundImage: (theme) => ({
+        home: "url('/images/homeHero.png')",
+      }),
     },
   },
   variants: {
