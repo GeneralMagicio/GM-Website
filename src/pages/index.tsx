5e1d90bf8cb1b8ed6d8a3722319ac8e8b15dbbb0
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-08-16 15:23:35 -0300

    html title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import Head from 'next/head'
 import { HomeHero } from '../components/Home/Hero'
 import { History } from '../components/Home/History'
 import { HomeRedirect } from '../components/Home/Redirect'
 
 export default function Home() {
   return (
-    <div className="divide-y divide-neutral-800">
-      <HomeHero />
-      <HomeRedirect />
-      <History />
-    </div>
+    <>
+      <Head>
+        <title>General Magic</title>
+      </Head>
+      <div className="divide-y divide-neutral-800">
+        <HomeHero />
+        <HomeRedirect />
+        <History />
+      </div>
+    </>
   )
 }
