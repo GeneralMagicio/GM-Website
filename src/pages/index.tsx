31b5d1593cc98490082e7119f8f6d15fa5372523
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-08-16 15:15:40 -0300

    wip:font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,13 @@
-import Head from 'next/head'
 import { HomeHero } from '../components/Home/Hero'
 import { History } from '../components/Home/History'
 import { HomeRedirect } from '../components/Home/Redirect'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/AkiraExpanded.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin=""
-        />
-      </Head>
-      <div className="divide-y divide-neutral-800">
-        <HomeHero />
-        <HomeRedirect />
-        <History />
-      </div>
-    </>
+    <div className="divide-y divide-neutral-800">
+      <HomeHero />
+      <HomeRedirect />
+      <History />
+    </div>
   )
 }
