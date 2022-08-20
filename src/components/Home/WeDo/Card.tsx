c8dd819f845c6a6188f5bf149fca1df61a4d4754
luizakp <luiza.puccinelli06@gmail.com>
2022-08-20 13:37:17 -0300

    what we do

---
+++ src/components/Home/WeDo/Card.tsx
@@ -0,0 +1,33 @@
+export interface WeDoCardProps {
+  title: string
+  description: string
+}
+
+export function WeDoCard({ title, description }: WeDoCardProps) {
+  return (
+    <div>
+      <div className="h-grid-line h-[1px]"></div>
+      <div className="mt-10">
+        <h2 className="font-akira text-[26px]">{title}</h2>
+        <p className="text-2xl mt-6">{description}</p>
+        <div className="flex my-6 items-center hover:underline">
+          <p className="uppercase text-magicPurple-300 mr-2">Learn more</p>
+          <div className="">
+            <svg
+              width="16"
+              height="10"
+              viewBox="0 0 16 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 4.70456C15.9934 4.53633 15.9242 4.34184 15.8088 4.21922L12.044 0.219116C11.789 -0.0101681 11.2997 -0.104706 11.0146 0.16036C10.7338 0.421485 10.7422 0.927667 11.0218 1.18977L13.6689 3.99866L0.70592 3.99866C0.31612 3.99866 0 4.31477 0 4.70458C0 5.09439 0.316107 5.4105 0.70592 5.4105L13.6689 5.4105L11.0218 8.21939C10.7809 8.46066 10.7364 8.98489 11.0146 9.2488C11.2926 9.51288 11.7993 9.42852 12.044 9.18988L15.8088 5.18978C15.9376 5.05289 16.0003 4.89221 16 4.70444V4.70456Z"
+                fill="#A569FF"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Home/WeDo/index.tsx
@@ -0,0 +1,49 @@
+import { Subtitle } from '../../base/Subtitle'
+import { Title } from '../../base/Title'
+import { WeDoCard, WeDoCardProps } from './Card'
+
+export function HomeWhatWeDo() {
+  const cards: WeDoCardProps[] = [
+    {
+      title: 'Dao',
+      description:
+        'We invest in DAOs such as Giveth, Commons Stack, TEC and more, and actively participate in their governance.',
+    },
+    {
+      title: 'r&d',
+      description:
+        'Our robust R&D team is developing groundbreaking technologies and making ongoing contributions to the Ethereum Ecosystem.',
+    },
+    {
+      title: 'education',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+  ]
+  return (
+    <div className="mt-44 mx-12 lg:mx-56 ">
+      <div className="flex flex-col md:flex-row items-center">
+        <div className="mr-24 grid md:grid-cols-2 gap-x-12 gap-y-[88px]">
+          <Title>
+            <span>What</span>
+            <span>we do</span>
+          </Title>
+          <Subtitle paddingBottom={false} paddingTop={false}>
+            General Magic is deploying resources to the most innovative
+            DAO&apos;s that are building social impact solutions throughout Web3
+            and Public Good space.
+          </Subtitle>
+          {cards.map((card) => {
+            return (
+              <WeDoCard
+                key={card.title}
+                title={card.title}
+                description={card.description}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
