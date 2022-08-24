e635cc2d33e262256bea8b1dd2454f2279866905
luizakp <luiza.puccinelli06@gmail.com>
2022-08-24 11:00:41 -0300

    members

---
+++ src/components/Home/OurMagicians/Card.tsx
@@ -0,0 +1,18 @@
+import Image, { StaticImageData } from 'next/image'
+import defaultUser from '../../../../public/images/members/default.png'
+
+export interface OurMagiciansCardProps {
+  name: string
+  role: string
+  image?: StaticImageData | string
+}
+
+export function OurMagiciansCard({ name, role, image }: OurMagiciansCardProps) {
+  return (
+    <div>
+      <Image src={image ? image : defaultUser} alt={name} />
+      <h2 className="font-akira text-[10px] md:text-[26px]">{name}</h2>
+      <p className="uppercase text-[7px] md:text-base">{role}</p>
+    </div>
+  )
+}
