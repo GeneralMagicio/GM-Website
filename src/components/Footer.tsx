d35c105be206220b89d332eaef7928c467c481d1
luizakp <luiza.puccinelli06@gmail.com>
2022-09-06 11:36:06 +0200

    mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,16 +49,21 @@ export function Footer() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </p>
-      <div className="grid md:grid-cols-4 grid-cols-3 mt-8 md:mt-0">
-        <div>
-          <Image src={GM} alt="General Magic" width={72} height={44} />
+      <div className="flex sm:hidden justify-center mt-8">
+        <Image src={GM} alt="General Magic" width={72} height={44} />
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 mt-8 md:mt-0">
+        <div className="sm:flex justify-center hidden">
+          <div>
+            <Image src={GM} alt="General Magic" width={72} height={44} />
+          </div>
         </div>
-        <div className="grid gap-y-6 h-fit">
+        <div className="grid gap-y-6 h-fit justify-center">
           {pages.map((page) => {
             return <p key={page.label}>{page.label}</p>
           })}
         </div>
-        <div className="grid gap-y-6 h-fit">
+        <div className="grid gap-y-6 h-fit justify-center">
           {socials.map((social) => {
             return <p key={social.label}>{social.label}</p>
           })}
