8c49474dd6dc5075c7dc5722e169990d4ae67e52
luizakp <luiza.puccinelli06@gmail.com>
2022-09-06 16:38:46 +0200

    entry nav animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { Disclosure } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import gmLogo from '../../public/images/gmLogo.svg'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
 
 export function Navbar() {
   const pages = [
@@ -31,6 +33,7 @@ export function Navbar() {
       url: '',
     },
   ]
+
   return (
     <Disclosure as="nav" className="bg-neutral-900 z-50 relative">
       {({ open }) => (
@@ -62,53 +65,63 @@ export function Navbar() {
                   </div>
                 </button>
               </div>
-              <div className="hidden md:grid grid-cols-6 h-full items-center md:gap-x-14 sm:px-5">
+              <div className="hidden md:grid grid-cols-6 h-full items-center md:gap-x-14 px-10">
                 {pages.map((page) => {
                   return (
                     <button
                       key={page.title}
-                      className="text-white hover:text-magicPurple-300 text-center sm:text-xs lg:text-base max-w-fit"
+                      className="text-white hover:text-magicPurple-300 text-center text-xs xl:text-base max-w-fit"
                     >
                       {page.title}
                     </button>
                   )
                 })}
               </div>
-              <button className="hidden uppercase bg-magicPurple-300 h-full md:flex items-center hover:bg-opacity-70">
-                <h1 className="text-white font-akira 2xl:px-11 xl:px-8 md:px-5 lg:text-base text-[8px]">
+              <button className="hidden uppercase bg-magicPurple-300 h-full md:flex items-center hover:bg-opacity-70 justify-center">
+                <h1 className="text-white font-akira 2xl:px-11 xl:px-8 md:px-5 xl:text-base text-[8px]">
                   work with us
                 </h1>
               </button>
             </div>
           </div>
-
-          <Disclosure.Panel className="md:hidden">
-            <div className="pt-2 pb-8 space-y-1 uppercase flex flex-col items-center">
-              <Link href="/">
-                <a className="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 block pl-3 pr-4 py-2 text-base font-medium">
-                  Home
-                </a>
-              </Link>
-              {pages.map((page) => {
-                return (
-                  <Link href="/" key={page.title}>
-                    <a className="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 block pl-3 pr-4 py-2 text-base font-medium">
-                      {page.title}
-                    </a>
-                  </Link>
-                )
-              })}
-              <Link href="/">
-                <div className="px-8 w-full">
-                  <div className="text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 border-transparent bg-magicPurple-300 w-full">
-                    <a className="font-akira  block pl-3 pr-4 py-3 text-base font-medium text-center">
-                      Work with us
-                    </a>
+          <AnimatePresence>
+            {open && (
+              <motion.div
+                key="nav"
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -20, opacity: 0 }}
+              >
+                <Disclosure.Panel className="md:hidden">
+                  <div className="pt-2 pb-8 space-y-1 uppercase flex flex-col items-center">
+                    <Link href="/">
+                      <a className="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 block pl-3 pr-4 py-2 text-base font-medium">
+                        Home
+                      </a>
+                    </Link>
+                    {pages.map((page) => {
+                      return (
+                        <Link href="/" key={page.title}>
+                          <a className="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 block pl-3 pr-4 py-2 text-base font-medium">
+                            {page.title}
+                          </a>
+                        </Link>
+                      )
+                    })}
+                    <Link href="/">
+                      <div className="px-8 w-full">
+                        <div className="text-white hover:bg-gray-50 hover:border-gray-300 hover:text-magicPurple-300 border-transparent bg-magicPurple-300 w-full">
+                          <a className="font-akira block pl-3 pr-4 py-3 text-base text-center">
+                            Work with us
+                          </a>
+                        </div>
+                      </div>
+                    </Link>
                   </div>
-                </div>
-              </Link>
-            </div>
-          </Disclosure.Panel>
+                </Disclosure.Panel>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </>
       )}
     </Disclosure>
