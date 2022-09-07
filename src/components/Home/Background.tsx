8714c8d851cc0d33fdacf29270cf840bfa607211
luizakp <luiza.puccinelli06@gmail.com>
2022-09-07 20:07:13 +0200

    minor fix

--- src/components/Home/Background.tsx
+++ src/components/Home/Background.tsx
@@ -9,7 +9,7 @@ export function HomeBg() {
       <div className="relative bg-pinkHomeBg bg-cover bg-center bg-no-repeat bg-pinkPotion-300 px-10 sm:px-16 md:px-32 py-10 md:py-28">
         <Build />
       </div>
-      <div className="min-h-screen bg-homeBg bg-cover bg-center bg-no-repeat bg-blend-darken bg-neutral-900 md:first-letter:pb-24 pb-16">
+      <div className="bg-homeBg bg-cover bg-center bg-no-repeat bg-blend-darken bg-neutral-900 md:first-letter:pb-24 pb-16">
         <Magic />
         <HomeWhatWeDo />
         <OurMagicians />
