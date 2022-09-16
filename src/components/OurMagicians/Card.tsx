00cba41918cd2763a5fb8df207c16c24cbf52bbd
luizakp <luiza.puccinelli06@gmail.com>
2022-09-16 16:16:38 +0200

    our members component

--- src/components/OurMagicians/Card.tsx
+++ src/components/OurMagicians/Card.tsx
@@ -1,5 +1,5 @@
 import Image, { StaticImageData } from 'next/image'
-import defaultUser from '../../../../public/images/members/default.png'
+import defaultUser from '../../../public/images/members/default.png'
 
 export interface OurMagiciansCardProps {
   name: string
@@ -9,7 +9,7 @@ export interface OurMagiciansCardProps {
 
 export function OurMagiciansCard({ name, role, image }: OurMagiciansCardProps) {
   return (
-    <div>
+    <div className="max-w-[345px]">
       <Image src={image ? image : defaultUser} alt={name} />
       <h2 className="font-akira text-[10px] md:text-[26px]">{name}</h2>
       <p className="uppercase text-[7px] md:text-base">{role}</p>
