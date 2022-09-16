fd5202c74feb280982822d862b85b103a6895f18
luizakp <luiza.puccinelli06@gmail.com>
2022-09-16 16:19:50 +0200

    our members props

--- src/components/Home/Background.tsx
+++ src/components/Home/Background.tsx
@@ -1,6 +1,6 @@
 import { Build } from './Build'
 import { Magic } from './OurMagic'
-import { OurMagicians } from './OurMagicians'
+import { OurMagicians } from '../OurMagicians'
 import { HomeWhatWeDo } from './WeDo'
 
 export function HomeBg() {
@@ -9,10 +9,10 @@ export function HomeBg() {
       <div className="relative bg-pinkHomeBg bg-cover bg-center bg-no-repeat bg-pinkPotion-300 px-10 sm:px-16 md:px-32 py-10 md:py-28">
         <Build />
       </div>
-      <div className="bg-homeBg bg-cover bg-center bg-no-repeat bg-blend-darken bg-neutral-900 md:first-letter:pb-24 pb-16">
+      <div className="bg-homeBg bg-cover bg-center bg-no-repeat bg-blend-darken bg-neutral-900 md:pb-24 pb-16">
         <Magic />
         <HomeWhatWeDo />
-        <OurMagicians />
+        <OurMagicians page="Home" />
       </div>
     </>
   )
