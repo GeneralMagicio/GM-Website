b75aca40ebc90b925f961bb37545b4ee8034f2ca
luizakp <luiza.puccinelli06@gmail.com>
2022-09-28 23:24:30 +0200

    fix values text

--- src/components/About/Values.tsx
+++ src/components/About/Values.tsx
@@ -20,7 +20,7 @@ export function AboutValues() {
       image: whyWeDoIt,
       imageAlt: 'How are funded',
       description:
-        '2Our Mission is to make magic happen! We do this by supporting crypto-philanthropic organizations and communities to reach their full potential, collaborating with them in a diverse range of capacities. The source of our magic thrives on the digital winds of the Blockchain4Good universe.',
+        'The ecosystem of Blockchain projects, on Ethereum in particular, is growing at a lightning pace. With the rapid development of DeFi and DeGov the world needs talented sorcerers and sorceresses to build for-good tools leveraging Web3 innovations. General Magic was born out of the Giveth Galaxy; Immersed in a culture of Blockchain Philanthropy, we have the knowledge and insights to build a better future for the Decentralized Web.',
       componentPosition: 'right',
     },
     {
@@ -29,7 +29,7 @@ export function AboutValues() {
       image: howAreWeFunded,
       imageAlt: 'How are funded',
       description:
-        '1Our Mission is to make magic happen! We do this by supporting crypto-philanthropic organizations and communities to reach their full potential, collaborating with them in a diverse range of capacities. The source of our magic thrives on the digital winds of the Blockchain4Good universe.',
+        'We are in the process of implementing a client payment-based model. We implement a combination of long-term work agreements with value-aligned DAOs and projects as well as stand-alone bounties, projects, and consultancy work to generate sustainable funding for General Magic.',
       componentPosition: 'left',
     },
   ]
