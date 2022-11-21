ae1745cc9135cbe85bd7ceb07e22792feaab4fa9
luizakp <luiza.puccinelli06@gmail.com>
2022-11-21 14:08:50 +0100

    add: up button to mobile

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { ReactElement } from 'react'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
+import { ButtonUp } from '@/components/base/ButtonUp'
 
 interface IAppLayout {
   children: ReactElement
 }
 
 export function AppLayout({ children }: IAppLayout) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
   return (
-    <div className="divide-y divide-neutral-800">
-      <Navbar />
-      {children}
-      <Footer />
+    <div className="relative">
+      <div className="divide-y divide-neutral-800">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+      <ButtonUp isVisible={isVisible} onClick={scrollToTop} />
     </div>
   )
 }
